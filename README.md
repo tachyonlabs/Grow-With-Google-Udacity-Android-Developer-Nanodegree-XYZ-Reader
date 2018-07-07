e72eccf788f087873486919c693910e37eb8e30d
Tane Tachyon <tachyon@tachyonlabs.com>
2018-07-06 20:18:56 -0700

    First commit, just to create the repo after getting the starter code working

---
+++ README.md
@@ -0,0 +1,5 @@
+# XYZ Reader - "Make Your App Material"
+
+When complete, this will be my submission for the sixth project in the "Grow With Google" 
+scholarship Udacity/Google Android Developer Nanodegree program. #GoogleUdacityScholars 
+#GrowWithGoogle #MadeWithUdacity
---
+++ app/src/main/java/com/tachyonlabs/xyzreader/data/ArticleLoader.java
@@ -0,0 +1,44 @@
+package com.tachyonlabs.xyzreader.data;
+
+import android.content.Context;
+import android.content.CursorLoader;
+import android.net.Uri;
+
+/**
+ * Helper for loading a list of articles or a single article.
+ */
+public class ArticleLoader extends CursorLoader {
+    public static ArticleLoader newAllArticlesInstance(Context context) {
+        return new ArticleLoader(context, com.tachyonlabs.xyzreader.data.ItemsContract.Items.buildDirUri());
+    }
+
+    public static ArticleLoader newInstanceForItemId(Context context, long itemId) {
+        return new ArticleLoader(context, com.tachyonlabs.xyzreader.data.ItemsContract.Items.buildItemUri(itemId));
+    }
+
+    private ArticleLoader(Context context, Uri uri) {
+        super(context, uri, Query.PROJECTION, null, null, com.tachyonlabs.xyzreader.data.ItemsContract.Items.DEFAULT_SORT);
+    }
+
+    public interface Query {
+        String[] PROJECTION = {
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items._ID,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.TITLE,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.PUBLISHED_DATE,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.AUTHOR,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.THUMB_URL,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.PHOTO_URL,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.ASPECT_RATIO,
+                com.tachyonlabs.xyzreader.data.ItemsContract.Items.BODY,
+        };
+
+        int _ID = 0;
+        int TITLE = 1;
+        int PUBLISHED_DATE = 2;
+        int AUTHOR = 3;
+        int THUMB_URL = 4;
+        int PHOTO_URL = 5;
+        int ASPECT_RATIO = 6;
+        int BODY = 7;
+    }
+}
